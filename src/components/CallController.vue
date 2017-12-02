1ee1530be8710136fee971e8239bde5f89f4818a
Luiz Fernando <luiiz_roodrigues@hotmail.com>
2017-12-02 02:00:22 -0200

    start css

--- src/components/CallController.vue
+++ src/components/CallController.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>
-    <input type="text" v-model="call.cep" placeholder="CEP">
-    <input type="text" v-model="call.telephone" placeholder="Telefone">
-    <input type="text" v-model="call.city" placeholder="Cidade">
-    <input type="text" v-model="call.adress" placeholder="Endereço">
-    <input type="text" v-model="call.number" placeholder="Número">
-    <input type="text" v-model="call.neighborhood" placeholder="Bairro">
+  <div id="container">
+    <div id="top">
+      <div id="locationData">
+
+        <input type="text" v-model="call.cep" placeholder="CEP">
+        <input type="text" v-model="call.telephone" placeholder="Telefone">
+        <input type="text" v-model="call.city" placeholder="Cidade">
+        <input type="text" v-model="call.adress" placeholder="Endereço">
+        <input type="text" v-model="call.number" placeholder="Número">
+        <input type="text" v-model="call.neighborhood" placeholder="Bairro">
+      </div>
+
+      <div id="personData">
+        <input type="text" v-model="call.requester" placeholder="Solicitante">
+
+        <input type="text" v-model="call.pacient" placeholder="Paciente">
+        <input type="text" v-model="call.gender" placeholder="Sexo">
+        <input type="text" v-model="call.age" placeholder="Idade">
+        <input type="text" v-model="call.complaint" placeholder="Queixa do paciente">
+
+      </div>
+
+    </div>
+      <input type="text" v-model="call.description" placeholder="Descrição da Ocorrência">
+    <div>
+
+    </div>
 
 
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
 
-    <input type="text" v-model="call.pacient" placeholder="Paciente">
-    <input type="text" v-model="call.gender" placeholder="Sexo">
-    <input type="text" v-model="call.age" placeholder="Idade">
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
-    <input type="text" v-model="call.requester" placeholder="Solicitante">
+
 
   </div>
 </template>
@@ -35,4 +46,27 @@
 </script>
 <style lang="scss" scoped>
 
+  #container{
+    display:flex;
+    justify-content:space-around;
+
+    input{
+      margin:4%;
+    }
+  }
+  #top{
+    display:flex;
+    flex-direction:row;
+    justify-content:space-around;
+  }
+  #personData{
+    display:flex;
+    flex-direction:column;
+
+  }
+
+  #locationData{
+    display:flex;
+    flex-direction:column;
+  }
 </style>

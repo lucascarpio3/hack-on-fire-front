8b5da5d075fe508b9f07dcd22180fa053889c3d2
Lucas Cárpio <lucas.carpio3@gmail.com>
2017-12-01 23:46:47 -0200

    firt steps

---
+++ src/components/CallController.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model="call.requester">
+    {{call.requester}}
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        call: {
+          requester: ''
+        }
+      }
+    }
+  }
+</script>
+<style scoped >
+
+</style>

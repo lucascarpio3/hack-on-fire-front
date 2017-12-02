2051e41903ea631f8ce14d58411af3fd8f601728
Luiz Fernando <luiiz_roodrigues@hotmail.com>
2017-12-02 06:50:02 -0200

    needmedicalcare

--- src/components/NeedMedicalCare.vue
+++ src/components/NeedMedicalCare.vue
@@ -1,31 +1,58 @@
 <template>
-  <div>
-    <div class="form-group">
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="checkbox" class="form-check-input">
-          Hemorragia
-        </label>
-
-      </div>
-
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="checkbox" class="form-check-input">
-          Check me
-        </label>
-      </div>
-
+  <div class="row">
+    <div class="col-md-3">
+      <input type="text" @keyup="searchHurtType" v-model="searchTerm" @keyup.enter="selectFirst">
+        <ul v-if="searchTerm.length > 0">
+          <li v-for="hType in hurtTypeFiltred ">{{hType}}</li>
+        </ul>
     </div>
 
+    <div class="col-md-9">
 
 
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "need-medical-care.vue"
+  import { removeDiacritics } from '../utils/tools.js'
+  export default {
+      name: 'needMedicalCare',
+      computed: {
+        hurtTypeFiltred: function () {
+          return this.hurtTypes.filter(value => removeDiacritics(value).toUpperCase().indexOf(removeDiacritics(this.searchTerm).toUpperCase()) >= 0)
+        }
+      },
+      methods: {
+        searchHurtType (val) {
+
+        },
+        selectFirst () {
+          this.hurtSelected = this.hurtTypeFiltred[0]
+
+        }
+
+      },
+      data () {
+        return {
+          hurtSelected: null,
+          searchTerm: '',
+          hurtTypes: [
+            'Hemorragia',
+            'Estado de Choque',
+            'Sindrome Coronariana',
+            'Trauma de Extremidade',
+            'Trauma Abdominal',
+            'Trauma Cranio Encefalico',
+            'Traumatismo Raquimedular',
+            'Trauma Toracico',
+            'Lesao Termica'
+
+          ]
+
+        }
+      }
+
     }
 </script>
 

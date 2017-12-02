09b257b2feac0c27e21d30af2390db0888ffaed4
marcelofilhogit <marcelofilhoufu@gmail.com>
2017-12-02 09:40:16 -0200

    Alteração na chamada

--- src/components/CallController.vue
+++ src/components/CallController.vue
@@ -55,6 +55,22 @@
           </div>
         </div></br>
 
+
+        <div>
+          <div>
+            Pacientes:
+            <ul class="patients">
+              <card v-for="(card,index) in cards" :key="index" :value="card"
+                    @change="card => cards[index] = card"
+                    @remove="cards.splice(index,1)"></card>
+              <button @click="addCard">+</button>
+            </ul>
+          </div>
+        </div>
+
+
+
+
         <div class="row btn_ocorrencia">
           <div class="col-md-12 mb-3">
             <button type="button" class="btn btn-primary">Cria Ocorrência</button>
@@ -71,83 +87,15 @@
 
 
 
-
-
-
-  <!--div class="container"></br>
-    <div class="jumbotron">
-      <h4 class="card-title">CHAMADA</h4>
-    </div>
-    <div class="from-grup">
-      <form>
-        <div class="row">
-          <div class="col-md-6 mb-4">
-            <label>Solicitante</label>
-            <input type="text" class="form-control" placeholder="Solicitante" v_molde="call.solicitante">
-          </div>
-          <div class="col-md-3 mb-4">
-            <label>Telefone 1</label>
-            <masked-input class="form-control" mask="(11) 1111-11111" v-model="call.telefone1"
-                          placeholder="Telefone Principal"></masked-input>
-          </div>
-          <div class="col-md-3 mb-4">
-            <label>Telefone 2</label>
-            <masked-input class="form-control" mask="(11) 1111-11111" v-model="call.telefone2"
-                          placeholder="Telefone Opcional"></masked-input>
-          </div>
-        </div>
-        </br>
-
-        <div class="row">
-          <div class="col-md-6 mb-4">
-            <label>Observação</label>
-            <input type="text" class="form-control" placeholder="Observação" v_molde="call.queixa">
-          </div>
-        </div>
-        </br>
-        <div class="row">
-          <div class="col-md-2 mb-4">
-            <label>CEP</label>
-            <masked-input class="form-control" placeholder="CEP" v_molde="call.cep" mask="11111-111"></masked-input>
-          </div>
-          <div class="col-md-4 mb-4">
-            <label>Municipio</label>
-            <input type="text" class="form-control" placeholder="Municipio" v_molde="call.municipio_id">
-          </div>
-          <div class="col-md-4 mb-4">
-            <label>Endereço</label>
-            <input type="text" class="form-control" placeholder="Endereço" v_molde="call.endereco">
-          </div>
-          <div class="col-md-2 mb-4">
-            <label>Número</label>
-            <input type="text" class="form-control" placeholder="Número" v_molde="call.numero">
-          </div>
-        </div>
-        </br>
-        <div class="row">
-          <div class="col-md-6 mb-4">
-            <label>Bairro</label>
-            <input type="text" class="form-control" v_model="call." placeholder="Bairro" v_molde="call.bairro">
-          </div>
-        </div>
-        </br>
-        <div class="row btn_ocorrencia">
-          <div class="col-md-12 mb-3">
-            <button type="button" class="btn btn-primary">Cria Ocorrência</button>
-          </div>
-        </div>
-        </br>
-      </form>
-    </div>
-    <pre>{{call}}</pre>
-  </div-->
 </template>
 <script>
   import MaskedInput from 'vue-masked-input'
+  import PatientCard from './PatientCard'
 
   export default {
     components: {
-      MaskedInput
+      MaskedInput,
+      Card: PatientCard
     },
     data () {
       return {
@@ -156,9 +104,44 @@
           telefone1: '',
           telefone2: ''
 
+        },
+
+        result: [],
+        cards: [
+          {
+            sexo: 'F',
+            idade: 18,
+            queixas: 'Dores fortes nos ombros.'
+          }
+        ]
+
+
+      }
+    },
+
+    mounted () {
+      console.log('teste')
+      this.$http.get('http://321b2b14.ngrok.io/api/v1/bairros').then(result => {
+        console.log(result)
+        this.result = result.body.data
+        if (!result.ok) {
+          this.result = result
         }
+      }, result => {
+        this.result = result
+      })
+    },
+    methods: {
+      addCard () {
+        this.cards.push(
+          {
+            sexo: 'M',
+            idade: 0,
+            queixas: 'test'
+          })
       }
     }
+
   }
 </script>
 <style lang="scss" scoped>
@@ -175,6 +158,11 @@
 
   .btn_ocorrencia{
     text-align: center;
+  }
+
+  .patients {
+    margin: 0 !important;
+    padding: 0 !important;
   }
 
 </style>

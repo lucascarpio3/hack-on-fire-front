a89a33f0135d7372e4dc0f7db603608d8c0dd927
Lucas Cárpio <lucas.carpio3@gmail.com>
2017-12-02 02:50:35 -0200

    layout css adjusts

--- src/components/CallController.vue
+++ src/components/CallController.vue
@@ -2,7 +2,6 @@
   <div id="container">
     <div id="top">
       <div id="locationData">
-
         <input type="text" v-model="call.cep" placeholder="CEP">
         <input type="text" v-model="call.telephone" placeholder="Telefone">
         <input type="text" v-model="call.city" placeholder="Cidade">
@@ -10,10 +9,8 @@
         <input type="text" v-model="call.number" placeholder="Número">
         <input type="text" v-model="call.neighborhood" placeholder="Bairro">
       </div>
-
       <div id="personData">
         <input type="text" v-model="call.requester" placeholder="Solicitante">
-
         <input type="text" v-model="call.pacient" placeholder="Paciente">
         <input type="text" v-model="call.gender" placeholder="Sexo">
         <input type="text" v-model="call.age" placeholder="Idade">
@@ -22,13 +19,12 @@
       </div>
 
     </div>
+    <div>
       <input type="text" v-model="call.description" placeholder="Descrição da Ocorrência">
+    </div>
     <div>
 
     </div>
-
-
-
 
 
   </div>
@@ -46,27 +42,43 @@
 </script>
 <style lang="scss" scoped>
 
-  #container{
-    display:flex;
-    justify-content:space-around;
-
-    input{
-      margin:4%;
+  #container {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    flex-grow: 1;
+    input {
+      margin: 4%;
     }
   }
-  #top{
-    display:flex;
-    flex-direction:row;
-    justify-content:space-around;
+
+  #top {
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    justify-content: space-around;
   }
-  #personData{
-    display:flex;
-    flex-direction:column;
+
+  #personData {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
 
   }
 
-  #locationData{
-    display:flex;
-    flex-direction:column;
+  #locationData {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+  }
+
+  input {
+    padding: 12px;
+    font-size: 12pt;
+    border: 1px solid #999;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    width: 100%;
   }
 </style>

5a91ea7723dd00fda143a301e058fa5f3d3b6beb
marcelofilho <marcelofilhoufu@gmail.com>
2017-12-02 01:20:05 -0200

    Dev Screen

--- src/components/CallController.vue
+++ src/components/CallController.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="form">
-    <input class="inp" type="text" v-model="call.requester">
-    {{call.requester}}
+    <input class="inp" type="text" v-model="call.requester" placeholder="Solicitante">
+    <input class="inp" type="text" v-model="call.telephone" placeholder="Telefone">
+    <input class="inp" type="text" v-model="call.city" placeholder="Municipio">
+    <input class="inp" type="text" v-model="call.address" placeholder="Endereço">
+    <input class="inp" type="text" v-model="call.number" placeholder="Número">
+    <input class="inp" type="text" v-model="call.neighborhood" placeholder="Bairro">
+    <input class="inp" type="text" v-model="call.reference" placeholder="Referênce">
+    <input class="inp" type="text" v-model="call.patient" placeholder="Paciente">
+    <input class="inp" type="text" v-model="call.sex" placeholder="Sexo">
+    <input class="inp" type="text" v-model="call.age" placeholder="Idade">
+    <input class="inp" type="text" v-model="call.complaint" placeholder="Queixa da paciente">
+    <input class="inp" type="text" v-model="call.comments" placeholder="Observações">
   </div>
 </template>
 <script>

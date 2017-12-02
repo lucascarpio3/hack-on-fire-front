1e98f429055ef7b4450e3d9b41d7d4aaf6094129
Luiz Fernando <luiiz_roodrigues@hotmail.com>
2017-12-02 08:24:04 -0200

    needmedicalcure more implements

--- src/components/NeedMedicalCare.vue
+++ src/components/NeedMedicalCare.vue
@@ -1,59 +1,73 @@
 <template>
   <div class="row">
-    <div class="col-md-3">
-      <input type="text" @keyup="searchHurtType" v-model="searchTerm" @keyup.enter="selectFirst">
-        <ul v-if="searchTerm.length > 0">
-          <li v-for="hType in hurtTypeFiltred ">{{hType}}</li>
-        </ul>
+    <div class="col-sm-3">
+      <input type="text" @keyup="searchHurtType" v-model="searchTerm" @keyup.enter="selectFirst" @focus="">
+      <ul v-if="searchTerm.length > 0">
+        <li v-for="hType in hurtTypeFiltred ">{{hType}}</li>
+      </ul>
     </div>
 
-    <div class="col-md-9">
-
+    <div class="col-sm-9">
+      {{hurtCurrent}}
+      <ul>
+        <li v-if="hurtSelecteds.length >= 1 " >{{hurtSelecteds}}</li>
+      </ul>
 
     </div>
   </div>
 </template>
 
 <script>
-  import { removeDiacritics } from '../utils/tools.js'
+  import {removeDiacritics} from '../utils/tools.js'
+
   export default {
-      name: 'needMedicalCare',
-      computed: {
-        hurtTypeFiltred: function () {
-          return this.hurtTypes.filter(value => removeDiacritics(value).toUpperCase().indexOf(removeDiacritics(this.searchTerm).toUpperCase()) >= 0)
-        }
-      },
-      methods: {
-        searchHurtType (val) {
-
-        },
-        selectFirst () {
-          this.hurtSelected = this.hurtTypeFiltred[0]
-
-        }
+    name: 'needMedicalCare',
+    computed: {
+      hurtTypeFiltred: function () {
+        return this.hurtTypes.filter(this.removeSelected).filter(this.filterByTerm)
+      }
+    },
+    methods: {
+      searchHurtType (val) {
 
       },
-      data () {
-        return {
-          hurtSelected: null,
-          searchTerm: '',
-          hurtTypes: [
-            'Hemorragia',
-            'Estado de Choque',
-            'Sindrome Coronariana',
-            'Trauma de Extremidade',
-            'Trauma Abdominal',
-            'Trauma Cranio Encefalico',
-            'Traumatismo Raquimedular',
-            'Trauma Toracico',
-            'Lesao Termica'
-
-          ]
-
+      filterByTerm (value) {
+        return removeDiacritics(value).toUpperCase().indexOf(removeDiacritics(this.searchTerm).toUpperCase()) >= 0
+      },
+      removeSelected (hurtType) {
+        return this.hurtSelecteds.indexOf(hurtType) < 0
+      },
+      selectFirst () {
+        if (this.searchTerm.length > 0) {
+          if (this.hurtCurrent) {
+            this.hurtSelecteds.unshift(this.hurtCurrent)
+          }
+          this.hurtCurrent = this.hurtTypeFiltred[0]
         }
       }
+    },
+    data () {
+      return {
+        hurtSelecteds: [],
+        hurtCurrent: null,
+        searchTerm: '',
+        hurtTypes: [
+          'Hemorragia',
+          'Estado de Choque',
+          'Sindrome Coronariana',
+          'Trauma de Extremidade',
+          'Trauma Abdominal',
+          'Trauma Cranio Encefalico',
+          'Traumatismo Raquimedular',
+          'Trauma Toracico',
+          'Lesao Termica'
 
+        ]
+
+      }
     }
+
+  }
 </script>
 
 <style scoped>

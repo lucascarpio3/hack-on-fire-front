939d2011f0c1fec850bfde01e021afc217bbfa11
Luiz Fernando <luiiz_roodrigues@hotmail.com>
2017-12-02 00:53:19 -0200

    adicionado alguns formularios

--- src/components/CallController.vue
+++ src/components/CallController.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
-    <input type="text" v-model="call.requester">
-    {{call.requester}}
+    <input type="text" v-model="call.cep" placeholder="CEP">
+    <input type="text" v-model="call.telephone" placeholder="Telefone">
+    <input type="text" v-model="call.city" placeholder="Cidade">
+    <input type="text" v-model="call.adress" placeholder="Endereço">
+    <input type="text" v-model="call.number" placeholder="Número">
+    <input type="text" v-model="call.neighborhood" placeholder="Bairro">
+
+
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+
+    <input type="text" v-model="call.pacient" placeholder="Paciente">
+    <input type="text" v-model="call.gender" placeholder="Sexo">
+    <input type="text" v-model="call.age" placeholder="Idade">
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+    <input type="text" v-model="call.requester" placeholder="Solicitante">
+
+
+
   </div>
 </template>
 <script>

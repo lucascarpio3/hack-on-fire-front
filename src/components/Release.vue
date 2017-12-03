e7c153013019ddc16ef1bab7419b7fb687de2320
Lucas Cárpio <lucas.carpio3@gmail.com>
2017-12-03 08:01:14 -0200

    últimos commits

--- src/components/Release.vue
+++ src/components/Release.vue
@@ -1,82 +1,83 @@
 <template>
-  <div class="container">
+  <div>
     <hk-header></hk-header>
 
-    <div class="linha">
-      <i class="fa fa-users" aria-hidden="true"></i> Chamadas
-    </div><br>
+    <div class="linha row">
+      <div class="col-xs-8"><i class="fa fa-phone" aria-hidden="true"></i> Chamadas</div>
+      <div class="col-xs-4"><i class="fa fa-truck" aria-hidden="true"></i> Recursos</div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="panel-group col-xs-8">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" href="#collapse1">Status: Em Andamento</a>
+            </h4>
+          </div>
 
-    <div class="panel-group">
-      <div class="panel panel-primary">
-        <div class="panel-heading">
-          <h4 class="panel-title">
-            <a data-toggle="collapse" href="#collapse1">Status: Em Andamento</a>
-          </h4>
-        </div>
+          <div id="collapse1" class="panel-collapse collapse">
+            <div class="panel-body">
+              <div class="from-grup">
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <label>Solicitante</label>
+                    <input type="text" class="form-control" placeholder="Solicitante" v-model="call.solicitante">
+                  </div>
+                  <div class="col-md-2 mb-4">
+                    <label>CEP</label>
+                    <masked-input class="form-control" placeholder="CEP" v-model="call.cep"
+                                  mask="11111-111"></masked-input>
+                  </div>
+                  <div class="col-md-4 mb-4">
+                    <label>Municipio</label>
+                    <v-select :options="municipiosToSelect" v-model="call.municipio_id"></v-select>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                  <div class="col-md-3 mb-4">
+                    <label>Telefone 1</label>
+                    <masked-input class="form-control" mask="(11) 11111-1111" v-model="call.telefone1"
+                                  placeholder="Telefone Principal"></masked-input>
+                  </div>
+                  <div class="col-md-3 mb-4">
+                    <label>Telefone 2</label>
+                    <masked-input class="form-control" mask="(11) 11111-1111" v-model="call.telefone2"
+                                  placeholder="Telefone Opcional"></masked-input>
+                  </div>
+                  <div class="col-md-4 mb-4">
+                    <label>Endereço</label>
+                    <input type="text" class="form-control" placeholder="Endereço" v-model="call.endereco">
+                  </div>
+                  <div class="col-md-2 mb-4">
+                    <label>Número</label>
+                    <input type="text" class="form-control" placeholder="Número" v-model="call.numero">
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <label>Observação</label>
+                    <input type="text" class="form-control" placeholder="Observação" v-model="call.observacao">
+                  </div>
 
-        <div id="collapse1" class="panel-collapse collapse">
-          <div class="panel-body">
-            <div class="from-grup">
-              <div class="row">
-                <div class="col-md-6 mb-4">
-                  <label>Solicitante</label>
-                  <input type="text" class="form-control" placeholder="Solicitante" v-model="call.solicitante">
+                  <div class="col-md-6 mb-4">
+                    <label>Bairro</label>
+                    <v-select :options="bairros" v-model="call.bairro_id"></v-select>
+                  </div>
                 </div>
-                <div class="col-md-2 mb-4">
-                  <label>CEP</label>
-                  <masked-input class="form-control" placeholder="CEP" v-model="call.cep" mask="11111-111"></masked-input>
-                </div>
-                <div class="col-md-4 mb-4">
-                  <label>Municipio</label>
-                  <v-select :options="municipiosToSelect" v-model="call.municipio_id"></v-select>
-                </div>
-              </div>
-              <br>
-              <div class="row">
-                <div class="col-md-3 mb-4">
-                  <label>Telefone 1</label>
-                  <masked-input class="form-control" mask="(11) 11111-1111" v-model="call.telefone1"
-                                placeholder="Telefone Principal"></masked-input>
-                </div>
-                <div class="col-md-3 mb-4">
-                  <label>Telefone 2</label>
-                  <masked-input class="form-control" mask="(11) 11111-1111" v-model="call.telefone2"
-                                placeholder="Telefone Opcional"></masked-input>
-                </div>
-                <div class="col-md-4 mb-4">
-                  <label>Endereço</label>
-                  <input type="text" class="form-control" placeholder="Endereço" v-model="call.endereco">
-                </div>
-                <div class="col-md-2 mb-4">
-                  <label>Número</label>
-                  <input type="text" class="form-control" placeholder="Número" v-model="call.numero">
-                </div>
-              </div>
-              <br>
-              <div class="row">
-                <div class="col-md-6 mb-4">
-                  <label>Observação</label>
-                  <input type="text" class="form-control" placeholder="Observação" v-model="call.observacao">
-                </div>
+                <br>
 
-                <div class="col-md-6 mb-4">
-                  <label>Bairro</label>
-                  <v-select :options="bairros" v-model="call.bairro_id"></v-select>
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <label>Prioridade da Chamada</label><br>
+                    <input type="radio" name="gender" v-model="call.prioridade" value="M"> Baixo
+                    <input type="radio" name="gender" v-model="call.prioridade" value="F"> Medio
+                    <input type="radio" name="gender" v-model="call.prioridade" value="F"> Alto
+                  </div>
                 </div>
-              </div>
-              <br>
-
-              <div class="row">
-                <div class="col-md-6 mb-4">
-                  <label>Prioridade da Chamada</label><br>
-                  <input type="radio" name="gender" v-model="call.prioridade" value="M"> Baixo
-                  <input type="radio" name="gender" v-model="call.prioridade" value="F"> Medio
-                  <input type="radio" name="gender" v-model="call.prioridade" value="F"> Alto
-                </div>
-              </div><br>
-
-
-              <div>
+                <br>
                 <div>
                   <label>Pacientes</label>
                   <ul class="patients">
@@ -88,21 +89,30 @@
                     </button>
                   </ul>
                 </div>
-              </div>
-              <div class="row btn_ocorrencia">
-                <div class="col-md-12 mb-3">
-                  <button type="button" class="btn btn-success" @click="sendCall">Cria Ocorrência</button>
+                <div>
+                  <label>Recursos</label>
+                  <v-select multiple :options="recursosToSelect"></v-select>
                 </div>
               </div>
-              <br>
-            </div>
-
-            <div class="panel-footer">
-              <button type="button" class="btn btn-primary">Editar</button>
-              <button type="button" class="btn btn-success">Encaminhar</button>
+              <div class="panel-footer">
+                <button type="button" class="btn btn-primary">Editar</button>
+                <button type="button" class="btn btn-success">Encaminhar</button>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="col-xs-4">
+        <ul class="recursos">
+          <li v-for="rec in recursos" :class="['card', 'thumbnail', 'bootsnipp-thumb', 'row',
+            {disponivel:rec.status_id === 1},
+            {indisponivel:rec.status_id === 0},
+            {'em-rota':rec.status_id === 2},
+            {'em-atendimento':rec.status_id === 3}]">
+            <div class="col-xs-6">{{rec.codigo}}</div>
+            <div class="col-xs-6"><b>{{rec.status}}</b></div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -125,6 +135,9 @@
     computed: {
       municipiosToSelect () {
         return this.municipios.map(item => ({label: item.nome, value: item.id}))
+      },
+      recursosToSelect () {
+        return this.recursos.filter(item => item.status_id === 1).map(item => ({label: item.codigo}))
       }
     },
     data () {
@@ -141,7 +154,66 @@
           pacientes: []
         },
         municipios: [],
-        bairros: []
+        bairros: [],
+        recursos: [
+          {
+            codigo: 'A-554450',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554451',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554452',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554454',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554456',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554450',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554451',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554452',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554454',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554456',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554457',
+            status: 'Disponível',
+            status_id: 1
+          }, {
+            codigo: 'A-554455',
+            status: 'Em rota',
+            status_id: 2
+          }, {
+            codigo: 'A-554453',
+            status: 'Em atendimento',
+            status_id: 3
+          }, {
+            codigo: 'A-554457',
+            status: 'Indisponível',
+            status_id: 0
+          }
+        ]
       }
     },
     methods: {
@@ -185,6 +257,34 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .recursos {
+    padding: 0;
+    margin: 0;
+    overflow-y: auto;
+    position: fixed;
+    right: 0;
+    width: 33%;
+    max-height: 500px;
+  }
 
+  .card {
+    margin: -1px;
+    padding: 10px;
+    font-size: 13pt;
+
+    &.indisponivel {
+      background: #FFC4D0;
+    }
+    &.em-rota {
+      background: #A9E5FD;
+    }
+    &.em-atendimento {
+      background: #FDFFC1;
+    }
+    &.disponivel {
+      background: #BFECC8;
+    }
+
+  }
 </style>
